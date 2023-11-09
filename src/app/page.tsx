9eb8e5897c576b29224c628661a692f36760ea0e
Rafhael Fitzpatrick <rafhaelfitzpatrick@gmail.com>
2023-11-08 21:28:47 -0300

    Start new format update 9

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,70 +10,78 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 export default function Home() {
   return (
-    <main className={styles.main} style={{ backgroundColor: '#f9faff', fontFamily: 'Lato' }}>
+    <main className={styles.main} style={{ backgroundColor: '#f9faff', }}>
       {/* <LandingPageComponent /> */}
       {/* <HeaderComponent /> */}
 
-
       <PersistentDrawerLeft />
+
       <Divider />
-      <div style={{
+
+      <div className={styles.contactPageComponent} style={{
         width: '100%',
-        backgroundColor: '#f9faff',
+        backgroundColor: '#2b2b2c',
         height: '100%',
-
+        display: 'flex', gap: '3em', alignItems: 'center', flexWrap: 'wrap'
       }}>
-
-        <div id="contact" className={styles.contactPageComponent} style={{ color: 'black', padding: '3em 6em' }}>
-          <div className={styles.contactPageSessionOneComponent}>
-
-            <div className={styles.contactFlexSessionComponent} style={{ display: 'flex', gap: '3em', alignItems: 'center', flexWrap: 'wrap' }}>
-
-              <div className={styles.contactFlexSessionOneComponent} style={{ display: 'flex', gap: '1em', flexDirection: 'column', }}>
-                <div>
-                  <h3>Endereço</h3>
-                  <p>The Square - Av. Dr. Antônio Gomes de Barros, 625 - Sala 712 - Jatiúca, Maceió - AL, 57036-001</p>
-                </div>
-                <div>
-                  <h3>Telefone</h3>
-                  <p>(82) 98724-1343</p>
-                </div>
-                <div>
-                  <h3>Email</h3>
-                  <p>contato@dravaleriasiqueira.com.br</p>
-                </div>
-              </div>
-
-              <div className={styles.contactFlexSessionTwoComponent} style={{ display: 'flex', flexDirection: 'column', gap: '1em' }}>
-                <h3>Redes Sociais:</h3>
-                <div style={{ display: 'flex', gap: '0.5em' }}>
-                  <InstagramIcon />
-                  <span>Instagram</span>
-                </div>
-
-                <div style={{ display: 'flex', gap: '0.5em' }}>
-                  <FacebookIcon />
-                  <span>Facebook</span>
-                </div>
-
-                <div style={{ display: 'flex', gap: '0.5em' }}>
-                  <WhatsAppIcon />
-                  <span>WhatsApp</span>
-                </div>
-
-                <div style={{ display: 'flex', gap: '0.5em' }}>
-                  <LinkedInIcon />
-                  <span>LinkedIn</span>
-                </div>
-
-              </div>
-
-            </div>
-
+        <div className={styles.contactFlexSessionOneComponent} style={{ display: 'flex', gap: '1em', flexDirection: 'column', }}>
+          <div className={styles.poxaZero}>
+            <h3>Endereço</h3>
+            <p><a href="#" style={{ textDecoration: 'none', color: 'rgb(223, 178, 66)' }}>The Square - Av. Dr. Antônio Gomes de Barros, 625 - Sala 712 - Jatiúca, Maceió - AL, 57036-001</a></p>
           </div>
+          <div>
+            <h3>Telefone</h3>
+            <p><a href="#" style={{ textDecoration: 'none', color: 'rgb(223, 178, 66)' }}>(82) 98724-1343</a></p>
+          </div>
+          <div>
+            <h3>Email</h3>
+            <p><a href="#" style={{ textDecoration: 'none', color: 'rgb(223, 178, 66)' }}>contato@dravaleriasiqueira.com.br</a></p>
+          </div>
+          <div>
+            {/* <h3>Redes Sociais:</h3> */}
+            {/* <div className={styles.contactFlexSessionTwoComponent} style={{ display: 'flex', gap: '0.25em' }}>
+              <div style={{ display: 'flex', gap: '0.5em' }}>
+                <InstagramIcon />
+              </div>
+              <div style={{ display: 'flex', gap: '0.5em' }}>
+                <FacebookIcon />
+              </div>
+              <div style={{ display: 'flex', gap: '0.5em' }}>
+                <WhatsAppIcon />
+              </div>
+              <div style={{ display: 'flex', gap: '0.5em' }}>
+                <LinkedInIcon />
+              </div>
+            </div> */}
+          </div>
+
         </div>
 
+        {/* <div className={styles.contactFlexSessionTwoComponent} style={{ display: 'flex', flexDirection: 'column', gap: '1em' }}>
+          <h3>Redes Sociais:</h3>
+          <div style={{ display: 'flex', gap: '0.5em' }}>
+            <InstagramIcon />
+            <span>Instagram</span>
+          </div>
+
+          <div style={{ display: 'flex', gap: '0.5em' }}>
+            <FacebookIcon />
+            <span>Facebook</span>
+          </div>
+
+          <div style={{ display: 'flex', gap: '0.5em' }}>
+            <WhatsAppIcon />
+            <span>WhatsApp</span>
+          </div>
+
+          <div style={{ display: 'flex', gap: '0.5em' }}>
+            <LinkedInIcon />
+            <span>LinkedIn</span>
+          </div>
+
+        </div> */}
       </div>
+
     </main>
   )
 }
