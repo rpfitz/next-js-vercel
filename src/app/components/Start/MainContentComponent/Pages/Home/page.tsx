be2ad490c157245298c52dc1a9167ae047ef458a
Rafhael Fitzpatrick <rafhaelfitzpatrick@gmail.com>
2023-11-11 02:34:29 -0300

    Modifying colors pt9

--- src/app/components/Start/MainContentComponent/Pages/Home/page.tsx
+++ src/app/components/Start/MainContentComponent/Pages/Home/page.tsx
@@ -11,15 +11,24 @@ import { home } from '@/config-app'
 let whatsAppSendMessageLink = `https://wa.me/5582987241343?text=`
 let iconWhatsAppMessage = `Olá Dra Valéria, tudo bem? Gostaria de marcar uma consulta.`
 
+const clientStyles = {
+  // mobile?
+  header1: { lineHeight: '0.5em', fontSize: '2.2em', margin: 0, padding: 0, },
+  // mobile?
+  header2: { lineHeight: '2em', fontSize: '2.3em', margin: 0, padding: 0, },
+  paragraph1: { fontSize: '1.5em' },
+
+}
+
 export default function HomePage() {
   return (
     <div id="home" className={styles.container}>
       <div className={styles.session1}>
         <div className={styles.headers}>
-          <Typography className={styles.header1} variant="h2" gutterBottom>{home.first_header}</Typography>
-          <Typography className={styles.header2} variant="h2" gutterBottom><strong>{home.second_header}</strong></Typography>
+          <Typography style={clientStyles.header1} variant="h2" gutterBottom>{home.first_header}</Typography>
+          <Typography style={clientStyles.header2} variant="h2" gutterBottom><strong>{home.second_header}</strong></Typography>
         </div>
-        <Typography className={styles.paragraph1} variant="body1" gutterBottom>{home.text_message}</Typography>
+        <Typography style={clientStyles.paragraph1} variant="body1" gutterBottom>{home.text_message}</Typography>
         <div className={styles.buttonSession1}>
           <Link href={`${whatsAppSendMessageLink}${iconWhatsAppMessage}`}><Button variant="contained">{home.button_message}</Button></Link>
         </div>
